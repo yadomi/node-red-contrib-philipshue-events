6ce4059917f235a00e5927c436d3d10c53500d36
Felix Yadomi <felix.yadomi@gmail.com>
2022-01-27 22:39:25 +0100

    Adds nodes status

--- src/philipshue-events/philipshue-events.ts
+++ src/philipshue-events/philipshue-events.ts
@@ -2,19 +2,43 @@ import { NodeInitializer, NodeConstructor } from "node-red";
 
 const nodeInit: NodeInitializer = (RED): void => {
   const node: NodeConstructor = function (config) {
+    const status = {
+      incoming: () => {
+        this.status({ fill: "blue", text: "Incoming..." });
+        setTimeout(status.listening, 1000);
+      },
+      disconected: () => {
+        this.status({ fill: "red", text: "Not connected" });
+      },
+      listening: () => {
+        this.status({ fill: "green", text: "Listening" });
+      },
+      error: () => {
+        this.status({ fill: "red", text: "Error" });
+      },
+    };
+
     RED.nodes.createNode(this, config);
+    status.disconected();
 
     this.bridge = RED.nodes.getNode(config.bridge);
-    this.bridge.emitter.on('event', events => {
+    if (!this.bridge) return;
+
+    status.listening();
+
+    this.bridge.emitter.on("event", (events) => {
+      status.incoming();
+
       for (const event of events) {
         this.send({ payload: event });
       }
-    })
+    });
 
-    this.bridge.emitter.on('error', error => {
+    this.bridge.emitter.on("error", (error) => {
+      status.error();
       this.send({ payload: error });
-    })
-  }
+    });
+  };
 
   RED.nodes.registerType("philipshue-events", node);
 };
